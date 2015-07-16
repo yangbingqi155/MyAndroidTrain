70c151c0ec947a089458132c1b55a5e252424de0
yangbinqi <yangbingqi155@hotmail.com>
2015-07-15 23:29:39 -0700

    add bottom buttom

--- FirstDemo/src/com/example/firstdemo/MainActivity.java
+++ FirstDemo/src/com/example/firstdemo/MainActivity.java
@@ -46,10 +46,6 @@ public class MainActivity extends ActionBarActivity {
     
     public void sendMessage(View view)
     {
-    	Intent intent=new Intent(this,DisplayMesaageActivity.class);
-    	EditText editText=(EditText)findViewById(R.id.edit_message);
-    	String message=editText.getText().toString();
-    	intent.putExtra(EXTRA_MESSAGE, message);
-    	startActivity(intent);
+    	
     }
 }
